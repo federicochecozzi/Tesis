88c927d7a65fabf7b6d07df008e7dec19e8d7641
federicochecozzi <tiamantex@hotmail.com>
2024-03-08 20:30:00 -0300

    Detección de outliers por PCA robusto

---
+++ Código de prueba/pca_tests3.R
@@ -0,0 +1,62 @@
+# In case of missing library rhdf5, uncomment following line for first run of the script. Not needed later
+# install.packages("rhdf5")
+library(rhdf5)
+library(tidyverse)
+library(rospca)
+
+# In case of insufficent memory, uncomment following line. Your disc will be used as buffer and data might be loaded
+# memory.limit(size=65536)
+
+# set number of spectra and path to the data files
+setwd(r"(D:\Tesis\Datasets)")     # selecting the directory containing the data files
+spectraCount <- 100   # selecting the number of spectra for each sample (maximum of 500), recommended 100
+
+##########################################
+# Train Data
+##########################################
+
+wavelengths <- as.data.frame(h5read(file = "train.h5", name = "Wavelengths")) # import wavelengths
+trainClass <- as.data.frame(h5read(file = "train.h5", name = "Class")) # import classes
+trainData <- h5read(file = "train.h5", name = "Spectra") # import spectra
+h5closeAll()
+
+##########################################
+# Reduce number of spectra per sample
+##########################################
+
+reddim <- function(x){
+  x <- x[1:spectraCount,]
+}
+trainData <- lapply(trainData,reddim)
+
+##########################################
+
+trainData <- as.data.frame(do.call('rbind',trainData))
+tempClass <- vector()
+redClass <- trainClass[(1):(spectraCount),]
+for (i in c(seq(500,49500,500))){
+  tempClass <- trainClass[(i+1):(i+spectraCount),]
+  redClass <-  append(redClass,tempClass)
+  
+}
+trainClass <- redClass
+##########################################
+
+rm(i, redClass, spectraCount, tempClass, reddim)
+gc()
+
+start_time <- Sys.time()
+
+pca <- prcomp(trainData[trainClass == 1,]) #1.736716 mins
+
+end_time <- Sys.time()
+
+end_time - start_time
+
+start_time <- Sys.time()
+
+pcar <- robpca(trainData[trainClass == 1,], ndir = 5000) #2.193185 mins
+
+end_time <- Sys.time()
+
+end_time - start_time
---
+++ Experimentos/Preprocesamiento/outlier_detection.R
@@ -0,0 +1,86 @@
+#problemas con SVD, no usar
+
+library(rhdf5)
+library(rospca)
+#library(mlr)
+
+
+# In case of insufficent memory, uncomment following line. Your disc will be used as buffer and data might be loaded
+# memory.limit(size=65536)
+
+# set number of spectra and path to the data files
+setwd(r"(D:\Tesis\Datasets)")    # selecting the directory containing the data files
+spectraCount <- 500   # selecting the number of spectra for each sample (maximum of 500), recommended 100
+
+##########################################
+# Train Data
+##########################################
+
+wavelengths <- as.data.frame(h5read(file = "train.h5", name = "Wavelengths")) # import wavelengths
+trainClass <- as.data.frame(h5read(file = "train.h5", name = "Class")) # import classes
+trainData <- h5read(file = "train.h5", name = "Spectra") # import spectra
+h5closeAll()
+
+##########################################
+# Reduce number of spectra per sample
+##########################################
+
+reddim <- function(x){
+  x <- x[1:spectraCount,]
+}
+trainData <- lapply(trainData,reddim)
+
+##########################################
+
+trainData <- as.data.frame(do.call('rbind',trainData))
+tempClass <- vector()
+redClass <- trainClass[(1):(spectraCount),]
+for (i in c(seq(500,49500,500))){
+  tempClass <- trainClass[(i+1):(i+spectraCount),]
+  redClass <-  append(redClass,tempClass)
+  
+}
+trainClass <- redClass
+
+##########################################
+
+rm(i, redClass, spectraCount, tempClass, reddim)
+gc()
+
+##########################################
+# End of loading script
+##########################################
+
+#Búsqueda de outliers
+
+#debug
+
+#start_time <- Sys.time()
+
+#pcar <- robpca(trainData[trainClass == 1,], ndir = 10)
+
+#pcar$flag.all #acá está un flag que detecta outliers
+
+#end_time <- Sys.time()
+
+#end_time - start_time
+
+start_time <- Sys.time()
+
+outlier.flag <- c()
+
+for (i in 1:12){
+  print(i)
+  pcar <- robpca(trainData[trainClass == i,], ndir = 5000)
+  
+  outlier.flag <- c(outlier.flag,pcar$flag.all) #acá está un flag que detecta outliers
+}
+
+keep.flag <- !outlier.flag
+
+end_time <- Sys.time()
+
+end_time - start_time #13.52352 hours
+
+#¿Directorio?
+#write.csv(keep.flag, file ="to_keep_downsampled.csv", row.names=FALSE)
