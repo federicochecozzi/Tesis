ed82b691e45f7a75d93a766852170a9ee620917a
federicochecozzi <tiamantex@hotmail.com>
2024-04-27 21:30:33 -0300

    script CV accuracy

---
+++ Experimentos/PLS_CV/pls_acc.R
@@ -0,0 +1,148 @@
+library(tidyverse)
+library(data.table)
+library(caret)
+library(mdatools)
+library(rhdf5)
+#library(mlr)
+
+
+# In case of insufficent memory, uncomment following line. Your disc will be used as buffer and data might be loaded
+# memory.limit(size=65536)
+
+# set number of spectra and path to the data files
+setwd(r"(D:\Tesis\Datasets)")    # selecting the directory containing the data files
+spectraCount <- 500   # selecting the number of spectra for each sample (maximum of 500), recommended 100
+
+##########################################
+# Train Data
+##########################################
+
+wavelengths <- as.data.frame(h5read(file = "train_downsampled.h5", name = "Wavelengths")) # import wavelengths
+trainClass <- as.data.frame(h5read(file = "train_downsampled.h5", name = "Class")) # import classes
+trainData <- h5read(file = "train_downsampled.h5", name = "Spectra") # import spectra
+h5closeAll()
+
+##########################################
+# Reduce number of spectra per sample
+##########################################
+
+reddim <- function(x){
+  x <- x[1:spectraCount,]
+}
+trainData <- lapply(trainData,reddim)
+
+##########################################
+
+trainData <- as.data.frame(do.call('rbind',trainData))
+tempClass <- vector()
+redClass <- trainClass[(1):(spectraCount),]
+for (i in c(seq(500,49500,500))){
+  tempClass <- trainClass[(i+1):(i+spectraCount),]
+  redClass <-  append(redClass,tempClass)
+  
+}#¿Puede cargarse hasta 50000?
+trainClass <- redClass
+
+##########################################
+# Test Data
+##########################################
+
+testData <- h5read(file = "test_downsampled.h5", name = "UNKNOWN") # import spectra
+h5closeAll()
+
+testClass <- fread("test_labels.csv", dec = ',',header = FALSE)
+testClass <- testClass$V1
+
+testData <- as.data.frame(do.call('rbind',testData))
+
+##########################################
+# Outlier Data
+##########################################
+#setwd(r"(D:\Tesis\Algunos resultados\outliers)")
+
+#outlier_list <- fread("outlier_list_euclidean.csv", dec = ',',header = TRUE)
+
+#trainData <- trainData[outlier_list$q99 == FALSE,]
+#trainClass <- trainClass[outlier_list$q99 == FALSE]
+
+##########################################
+
+rm(i, redClass, spectraCount, tempClass, reddim)
+gc()
+
+##########################################
+# End of loading script
+##########################################
+
+start_time <- Sys.time()
+#model <- plsda(trainData,as.factor(trainClass),cv = list('ven', 5),ncomp = 60)
+#v[seq(1,100,1)%%5 == 1]
+#c = confusionMatrix(as.factor(c(1,2,3)), as.factor(c(1,3,2)))
+#c$overall['Accuracy']
+acc_matrix <- matrix(c(), nrow = 5, ncol = 60)
+index <- seq(1,49500,1)
+for (i in 0:4) {
+  model <- plsda(trainData[index[seq(1,49500,1)%%5 != i]],as.factor(trainClass[index[seq(1,49500,1)%%5 != i]]),cv = NULL,ncomp = 60)
+  labels <- as.factor(trainClass[index[seq(1,49500,1)%%5 == i]])
+  test_res <- predict(model, trainData[index[seq(1,49500,1)%%5 == i]], labels)
+  #pred_class <- test_res$c.pred
+  ypred <- test_res$y.pred
+  
+  #Cuidado con las clases faltantes, puede que se requiera llenar niveles
+  #testClassfactor <- as.factor(testClass)
+  #levels(testClassfactor) <- c(levels(testClassfactor),'12') 
+  #usar setdiff si vas a copiar esto, orden de los factores no importa
+  for (j in 1:60){
+    print("i = ",i,", j = ",j)
+    confmat <- confusionMatrix(labels,as.factor(apply(ypred[,j,],1,which.max))) 
+    if(i){
+      acc_matrix[i,j] <- confmat$overall['Accuracy']
+    } else {
+      acc_matrix[5,j] <- confmat$overall['Accuracy']
+    }
+  }
+}
+end_time <- Sys.time()
+
+end_time - start_time
+#7.533855 hours
+
+ggplot() %>%
+  geom_line(aes(x = 1:60, y = apply(acc_matrix, 2, mean)))
+#as.data.frame(acc_matrix) %>%
+
+#summary(model)
+
+#plotRMSE(model)
+
+#plotPredictions(model, ncomp = 60)
+#plotPredictions(structure(model, class = "regmodel"), ncomp = 14, ny = 3)
+
+#start_time <- Sys.time()
+#test_res = predict(model, testData, as.factor(testClass), ncomp = 60)
+#end_time <- Sys.time()
+
+#end_time - start_time
+#8.567268 mins
+
+#summary(test_res)
+
+#plotPredictions(test_res, ncomp = 60)
+
+#plotXResiduals(test_res, ncomp = 60)
+
+#confmat <- getConfusionMatrix(test_res, ncomp = 60)
+#confmat
+
+#accuracy <- sum(diag(confmat))/sum(confmat) * 100
+#accuracy
+#57.28198
+
+#showPredictions(test_res, ncomp = 14)
+
+#pred_class <- test_res$c.pred
+#ypred <- test_res$y.pred
+
+#testClassfactor <- as.factor(testClass)
+#levels(testClassfactor) <- c(levels(testClassfactor),'12')
+#confusionMatrix(as.factor(testClass),as.factor(apply(ypred[,60,],1,which.max))) #73.42
