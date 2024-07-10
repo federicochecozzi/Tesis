bc434f2d202c2a937a80309ff7266e34b67fbb68
federicochecozzi <tiamantex@hotmail.com>
2024-07-09 23:31:02 -0300

    Scripts usando pcout

---
+++ Experimentos/Preprocesamiento/outlier_list_pcout.R
@@ -0,0 +1,67 @@
+library(rhdf5)
+library(tidyverse)
+library(data.table)
+library(mvoutlier)
+
+# In case of insufficent memory, uncomment following line. Your disc will be used as buffer and data might be loaded
+# memory.limit(size=65536)
+
+# set number of spectra and path to the data files
+setwd(r"(D:\Tesis\Datasets)")    # selecting the directory containing the data files
+spectraCount <- 500   # selecting the number of spectra for each sample (maximum of 500), recommended 100
+
+##########################################
+# Train Data
+##########################################
+
+wavelengths <- as.data.frame(h5read(file = "train_processed.h5", name = "Wavelengths")) # import wavelengths
+trainClass <- as.data.frame(h5read(file = "train_processed.h5", name = "Class")) # import classes
+trainData <- h5read(file = "train_processed.h5", name = "Spectra") # import spectra
+h5closeAll()
+
+##########################################
+# Reduce number of spectra per sample
+##########################################
+
+reddim <- function(x){
+  x <- x[1:spectraCount,]
+}
+trainData <- lapply(trainData,reddim)
+
+##########################################
+
+trainData <- as.data.table(do.call('rbind',trainData))
+tempClass <- vector()
+redClass <- trainClass[(1):(spectraCount),]
+for (i in c(seq(500,49500,500))){
+  tempClass <- trainClass[(i+1):(i+spectraCount),]
+  redClass <-  append(redClass,tempClass)
+  
+}
+trainClass <- redClass
+
+##########################################
+
+rm(i, redClass, spectraCount, tempClass, reddim)
+gc()
+
+##########################################
+# End of loading script
+##########################################
+
+setwd(r"(D:\Tesis\Algunos resultados\outliers)")
+
+pcoutlier = c()
+
+for(c in 1:12){
+  print(c)
+  subset <- trainData[trainClass == c,]
+  result <- pcout(subset)
+  
+  pcoutlier = c(pcoutlier, result$wfinal < 0.25)
+}
+
+fwrite(list(pcoutlier = pcoutlier),
+       file = "outlier_list_pcout.csv",
+       sep = ","
+)
