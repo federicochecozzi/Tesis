5fe86c0d82434574cddaf5e1ede247c17fe64758
federicochecozzi <tiamantex@hotmail.com>
2025-08-25 17:41:34 -0300

    Script pls con base de datos FeCo completo

--- Código de prueba/pls_FeCo.R
+++ Código de prueba/pls_FeCo.R
@@ -14,13 +14,15 @@ Target <- as.data.table(t(h5read(file = "catalina_processed_and_downsampled_with
 
 #lidiar con los nombres de variables!
 
-trainData <- Data[Target$sample %in% c(1,2,3,4,6,7,8,9,10),]
-trainTarget <- Target[sample %in% c(1,2,3,4,6,7,8,9,10),.(Fe_comp, Co_comp)]
+#V1: sample, V2,V3: Fe_comp, Co_comp
+samples <- c(3,8)
+trainData <- Data[Target$V1 %in% samples,]
+trainTarget <- Target[V1 %in% samples,.(V2, V3)]
 
-testData <- Data[!Target$sample %in% c(1,2,3,4,6,7,8,9,10),]
-testTarget <- Target[!sample %in% c(1,2,3,4,6,7,8,9,10),.(Fe_comp, Co_comp)]
+testData <- Data[!Target$V1 %in% samples,]
+testTarget <- Target[!V1 %in% samples,.(V2, V3)]
 
-model = pls(trainData, trainTarget, cv = NULL, x.test = testData, y.test = testTarget)
+model = pls(as.matrix(trainData), as.matrix(trainTarget), cv = NULL, x.test = as.matrix(testData), y.test = as.matrix(testTarget))
 #model = selectCompNum(model, 2)
 summary(model)
 plot(model)
